9a6271592ea9ed7cfe4e0da46310a4f82a4d4992
doubleaway0421 <doubleaway@gmail.com>
2024-10-15 22:13:02 +0900

    [add] 레이아웃 폴더 추가 및 home 파일 추가하여 테스트 완

---
+++ front/layout/home.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native';
+/* 2024-10-15 
+홈 화면 레이아웃
+- 투두 리스트
+- 메뉴바
+- 팝업(미정)
+# 투두리스트가 많아지면 스크롤 (메뉴 미포)
+ */
+const Home = () => {
+    return (
+        <View>
+            <Text>home페이지</Text>
+        </View>);
+}
+
+export default Home
